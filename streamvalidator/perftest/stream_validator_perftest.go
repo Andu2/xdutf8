e78a401c9cfb901b96c40806fe229b9f6cc5060a
Andy Iverson <andy@andyiverson.me>
2026-03-06 13:44:52 -0600

    Some performance improvements

--- streamvalidator/perftest/stream_validator_perftest.go
+++ streamvalidator/perftest/stream_validator_perftest.go
@@ -9,26 +9,41 @@ import (
 )
 
 func main() {
-	testInput := []byte("The quick brown fox jumps over the lazy dog. 日本語テスト. Ünïcödé. 🔥🎉The quick brown fox jumps over the lazy dog. 日本語テスト. Ünïcödé. 🔥🎉The quick brown fox jumps over the lazy dog. 日本語テスト. Ünïcödé. 🔥🎉The quick brown fox jumps over the lazy dog. 日本語テスト. Ünïcödé. 🔥🎉The quick brown fox jumps over the lazy dog. 日本語テスト. Ünïcödé. 🔥🎉The quick brown fox jumps over the lazy dog. 日本語テスト. Ünïcödé. 🔥🎉The quick brown fox jumps over the lazy dog. 日本語テスト. Ünïcödé. 🔥🎉The quick brown fox jumps over the lazy dog. 日本語テスト. Ünïcödé. 🔥🎉The quick brown fox jumps over the lazy dog. 日本語テスト. Ünïcödé. 🔥🎉")
+	tests := []struct {
+		name string
+		data []byte
+	}{{
+		name: "ASCII",
+		data: []byte("mK#9vQ!zR@2pL$wX&8nJ%4tY^6fH*0dG~3sB+7cA=1eM<5uN>9oP|2iC-6kT/4lV.7bW,8gZ?3hD;0jF:5rE"),
+	}, {
+		name: "Mixed Length Unicode",
+		data: []byte("㸽㢐╧̨ƺǙ𐁝𐊘ęƋ𐋤᫃ᣭơỹ𐌊ÙΝĲᶊ࿽ሷηᗇɰ𐂒ࡪ᷆𐁁˾𐎠ᇳÛĨˮ㮒𐂎𐈢𐇕𐊆̛⩑𐎲ɭʹ𐁲𐅎𐂣˅⌌𐋫𐃎ഋ㯍㮧Ŏ𐈹ᧈ͎⺣㾢à𐌉㚳ຊ෯𐈤𐅍ƮƟ̜Ƥζ𐇁⭔𐅞Ύ㣝ะ𐋶ḃ𐇜𐇵𐆉𐇒ʋÞǜğે𐋗⁐‹𐆭𐌵㒮𐈨𐉐͙β"),
+	}, {
+		name: "Mixed ASCII and Unicode",
+		data: []byte("𐄽#~p6T-=)VH;2ɠ7{𐏤<$0Ū𐍢>E㽖fGᖮv*ó[R:'bhoσ↪y(UAzLC𐌟ீOX}|t_✔?suS˩%gÏ⛎K.⁁]nƲPd9lMDğ͛Zm@e኉!&/Q𐎣Y𐀄a𐉈I𐈟Fcixr"),
+	}}
 
 	const iterations = 100_000
 
-	start := time.Now()
-	for i := 0; i < iterations; i++ {
-		validator := streamvalidator.NewUtf8StreamValidator()
-		validator.Validate(testInput)
-	}
-	streamDuration := time.Since(start)
+	for _, test := range tests {
+		start := time.Now()
+		for i := 0; i < iterations; i++ {
+			validator := streamvalidator.NewUtf8StreamValidator()
+			validator.Validate(test.data)
+		}
+		streamDuration := time.Since(start)
 
-	start = time.Now()
-	for i := 0; i < iterations; i++ {
-		utf8.Valid(testInput)
-	}
-	stdlibDuration := time.Since(start)
+		start = time.Now()
+		for i := 0; i < iterations; i++ {
+			utf8.Valid(test.data)
+		}
+		stdlibDuration := time.Since(start)
 
-	fmt.Printf("Input length:     %d bytes\n", len(testInput))
-	fmt.Printf("Iterations:       %d\n", iterations)
-	fmt.Printf("Stream validator: %v (%.0f ns/op)\n", streamDuration, float64(streamDuration.Nanoseconds())/iterations)
-	fmt.Printf("Standard library: %v (%.0f ns/op)\n", stdlibDuration, float64(stdlibDuration.Nanoseconds())/iterations)
-	fmt.Printf("Ratio:            %.1fx\n", float64(stdlibDuration)/float64(streamDuration))
+		fmt.Printf("---%s---\n", test.name)
+		fmt.Printf("Input length:     %d bytes\n", len(test.data))
+		fmt.Printf("Iterations:       %d\n", iterations)
+		fmt.Printf("Stream validator: %v (%.0f ns/op)\n", streamDuration, float64(streamDuration.Nanoseconds())/iterations)
+		fmt.Printf("Standard library: %v (%.0f ns/op)\n", stdlibDuration, float64(stdlibDuration.Nanoseconds())/iterations)
+		fmt.Printf("Ratio:            %.2fx\n", float64(stdlibDuration)/float64(streamDuration))
+	}
 }
--- streamvalidator/stream_validator.go
+++ streamvalidator/stream_validator.go
@@ -8,43 +8,59 @@ type streamValidatorState struct {
 	overlongBitsToCheck uint8
 }
 
-const (
-	bytesLeftMask           = 3 << 0
-	overlongBitsToCheckMask = 3 << 2
-	checkReservedMask       = 1 << 4
-	checkMaxUnicodeMask     = 1 << 5
-	isInvalidMask           = 1 << 6
-)
-
 func NewUtf8StreamValidator() *streamValidatorState {
 	return &streamValidatorState{}
 }
 
-func (validator *streamValidatorState) Reset() {
-	validator.bytesLeft = 0
-	validator.checkReserved = false
-	validator.checkMaxUnicode = false
-	validator.isInvalid = false
-	validator.overlongBitsToCheck = 0
+func (state *streamValidatorState) Reset() {
+	state.bytesLeft = 0
+	state.checkReserved = false
+	state.checkMaxUnicode = false
+	state.isInvalid = false
+	state.overlongBitsToCheck = 0
 }
 
-func (validator *streamValidatorState) IsComplete() bool {
-	return validator.bytesLeft == 0
+func (state *streamValidatorState) IsComplete() bool {
+	return state.bytesLeft == 0
 }
 
-func (validator *streamValidatorState) Validate(octets []byte) (bool, string) {
-	if validator.isInvalid {
+func (state *streamValidatorState) Validate(octets []byte) (bool, string) {
+	if state.isInvalid {
 		return false, "previously found invalid"
 	}
 
-	for i := 0; i < len(octets); i++ {
-		octet := octets[i]
-		if validator.bytesLeft == 0 {
-			if octet&0b10000000 == 0 {
-				// ASCII
-				continue
-			} else if octet&0b11000000 == 0b10000000 {
-				validator.isInvalid = true
+	i := 0
+	for i < len(octets) {
+		if state.bytesLeft == 0 {
+			// Check ASCII first
+			// Fast path: Try to skip runs of 8 bytes at a time
+			for i+8 <= len(octets) {
+				// The following is copied from the standard library at unicode/utf8/utf8.go
+
+				// Combining two 32 bit loads allows the same code to be used
+				// for 32 and 64 bit platforms.
+				// The compiler can generate a 32bit load for first32 and second32
+				// on many platforms. See test/codegen/memcombine.go.
+				first32 := uint32(octets[i+0]) | uint32(octets[i+1])<<8 | uint32(octets[i+2])<<16 | uint32(octets[i+3])<<24
+				second32 := uint32(octets[i+4]) | uint32(octets[i+5])<<8 | uint32(octets[i+6])<<16 | uint32(octets[i+7])<<24
+				if (first32|second32)&0x80808080 != 0 {
+					break
+				}
+
+				i += 8
+			}
+
+			// Continue to try to skip through ASCII without having to check bytesLeft again
+			for i < len(octets) && octets[i]&0b10000000 == 0 {
+				i++
+			}
+			if i >= len(octets) {
+				break
+			}
+
+			octet := octets[i]
+			if octet&0b11000000 == 0b10000000 {
+				state.isInvalid = true
 				return false, "continuation octet without character start"
 			} else {
 				// start of char
@@ -52,76 +68,86 @@ func (validator *streamValidatorState) Validate(octets []byte) (bool, string) {
 					if octet&0b00011110 == 0 {
 						// This is the only overlong case that we can determine from the first byte
 						// (also, the last bit may be 1 and it is still overlong)
-						validator.isInvalid = true
+						state.isInvalid = true
 						return false, "overlong ascii character"
 					}
 
-					validator.bytesLeft = 1
+					state.bytesLeft = 1
 				} else if octet&0b11110000 == 0b11100000 { // length 3
-					validator.bytesLeft = 2
+					state.bytesLeft = 2
 					if octet&0b00001111 == 0 {
-						validator.overlongBitsToCheck = 1
+						state.overlongBitsToCheck = 1
 					}
 					if octet == 0b11101101 {
 						// The unicode values U+D800 through U+DFFF are reserved for utf-16 and invalid in utf-8.
 						// If we have a 3 byte char and the starting four bits are 1101 (D), we need to check the next bit.
 						// If the next bit of the char is a 1, it's invalid.
-						validator.checkReserved = true
+						state.checkReserved = true
 					}
 				} else if octet&0b11111000 == 0b11110000 { // length 4
 					if octet&0b00000100 == 0b00000100 {
 						// If any of the next 4 bits are 1, it's invalid because it's too large
 						// We can check the first two here, but have to set a flag to check the next two in the next byte
 						if octet&0b00000011 > 0 {
-							validator.isInvalid = true
+							state.isInvalid = true
 							return false, "exceeds max unicode value"
 						}
-						validator.checkMaxUnicode = true
+						state.checkMaxUnicode = true
 					}
 
-					validator.bytesLeft = 3
+					state.bytesLeft = 3
 					if octet&0b00000111 == 0 {
-						validator.overlongBitsToCheck = 2
+						state.overlongBitsToCheck = 2
 					}
 				} else {
 					// utf-8 was designed to allow up to 6 byte characters,
 					// but it was later decided that it is only allowed to go up to U+10FFFF
 					// which only requires 4 bytes
-					validator.isInvalid = true
+					state.isInvalid = true
 					return false, "character is more than 4 bytes"
 				}
 			}
 		} else {
+			octet := octets[i]
 			// continuation
 			if octet&0b11000000 != 0b10000000 {
-				validator.isInvalid = true
+				state.isInvalid = true
 				return false, "character started before previous finished"
 			}
-			if validator.checkReserved {
-				if octet&0b00100000 == 0b00100000 {
-					// This is invalid because it is a reserved value. See above where checkReserved is set to true.
-					validator.isInvalid = true
-					return false, "utf16 surrogate half"
+			// Doing this makes branch prediction better, I guess. It makes it slightly faster
+			if state.checkReserved || state.checkMaxUnicode || state.overlongBitsToCheck > 0 {
+				if state.checkReserved {
+					if octet&0b00100000 == 0b00100000 {
+						// This is invalid because it is a reserved value. See above where checkReserved is set to true.
+						state.isInvalid = true
+						return false, "utf16 surrogate half"
+					}
+					state.checkReserved = false
 				}
-				validator.checkReserved = false
-			}
-			if validator.checkMaxUnicode {
-				if octet&0b00110000 > 0 {
-					validator.isInvalid = true
-					return false, "exceeds max unicode value"
+				if state.checkMaxUnicode {
+					if octet&0b00110000 > 0 {
+						state.isInvalid = true
+						return false, "exceeds max unicode value"
+					}
+					state.checkMaxUnicode = false
 				}
-				validator.checkMaxUnicode = false
-			}
-			if validator.overlongBitsToCheck > 0 {
-				checkBits := octet << 1 >> (7 - validator.overlongBitsToCheck)
-				if checkBits == 0 {
-					validator.isInvalid = true
-					return false, "overlong character"
+				if state.overlongBitsToCheck > 0 {
+					var checkMask byte
+					if state.overlongBitsToCheck == 1 {
+						checkMask = 0b00100000
+					} else if state.overlongBitsToCheck == 2 {
+						checkMask = 0b00110000
+					}
+					if octet&checkMask == 0 {
+						state.isInvalid = true
+						return false, "overlong character"
+					}
+					state.overlongBitsToCheck = 0
 				}
-				validator.overlongBitsToCheck = 0
 			}
-			validator.bytesLeft -= 1
+			state.bytesLeft -= 1
 		}
+		i++
 	}
 
 	return true, ""
